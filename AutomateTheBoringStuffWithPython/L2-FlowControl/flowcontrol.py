589c7d2daed3a29299f7d980d6b4edc50e0361ba
Ricardo Hernandez <toto@Ricardos-MacBook-Air.local>
2021-08-12 15:07:32 -0600

    atbs w/python

---
+++ AutomateTheBoringStuffWithPython/L2-FlowControl/flowcontrol.py
@@ -0,0 +1,21 @@
+# name = 'Bob'
+# if name == 'Alice':
+#     print("Hi, Alice")
+# print('Done')
+
+# password = 'anakyn'
+# if password == 'anakyn':
+#     print('Access granted')
+# else:
+#     print('wrong password')
+
+
+span = 0
+while span < 5:
+    print('while')
+    span = span + 1
+
+span = 0
+if span < 5:
+    print('if')
+    span = span + 1
